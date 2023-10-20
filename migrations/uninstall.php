fc54bc883895abbed413979f05ff7750230d56a1
Yuriy Bakhtin <yurybakh@gmail.com>
2023-10-20 17:54:05 +0200

    Add migrations uninstall

---
+++ migrations/uninstall.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+use humhub\components\Migration;
+
+class uninstall extends Migration
+{
+    public function up()
+    {
+        $this->safeDropTable('question_answer_vote');
+        $this->safeDropTable('question_answer');
+        $this->safeDropTable('question');
+    }
+
+    public function down()
+    {
+        echo "uninstall does not support migration down.\n";
+        return false;
+    }
+}
