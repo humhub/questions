a4ad9a7e3ae93ff38930317a19098a069b83b009
Felix Hahn <info@hahn-felix.de>
2025-04-25 21:37:44 +0200

    remove card, use panel again

--- views/settings/form.php
+++ views/settings/form.php
@@ -8,19 +8,18 @@
 use humhub\modules\questions\models\forms\ContainerSettings;
 use humhub\modules\space\models\Space;
 use humhub\widgets\bootstrap\Button;
-use humhub\widgets\form\ActiveForm;
+use yii\bootstrap\ActiveForm;
 
 /* @var ContainerSettings $settings */
 ?>
-<div class="card">
-    <div class="card-header"><?= Yii::t('QuestionsModule.base', '<strong>Questions</strong> settings') ?></div>
-    <div class="card-body">
+<div class="panel panel-default">
+    <div class="panel-heading"><?= Yii::t('QuestionsModule.base', '<strong>Questions</strong> settings') ?></div>
+    <div class="panel-body">
         <div class="text-body-secondary">
             <?= $settings->contentContainer instanceof Space
                 ? Yii::t('QuestionsModule.base', 'Settings of the "Questions" module for this single Space.')
                 : Yii::t('QuestionsModule.base', 'Settings of the "Questions" module for your Profile.')?>
         </div>
-        <br>
         <?php $form = ActiveForm::begin() ?>
 
         <?= $form->field($settings, 'showAnswersInStream')->checkbox() ?>
